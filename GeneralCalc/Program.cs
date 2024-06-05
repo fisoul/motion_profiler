e5fc90694dc416b10a01f3fa4845a96dc43697dd
wyq-Mac <wang_yanqi@outlook.com>
2024-06-05 16:27:44 +0800

    afternoon break

--- GeneralCalc/Program.cs
+++ GeneralCalc/Program.cs
@@ -1,7 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
 
 Console.WriteLine("Hello, World!");
 
 var ret = Integrate.OnClosedInterval((x) => x, 0, 10);
 Console.WriteLine(ret);
+
